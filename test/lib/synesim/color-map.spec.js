ca615aba9303e678497d1d13cfaf93c8ecc033ae
Andrew Strader <reykjavikingur@gmail.com>
2016-06-26 21:21:16 -0400

    create data conversion methods for color map

--- test/lib/synesim/color-map.spec.js
+++ test/lib/synesim/color-map.spec.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var ColorMap = require('synesim/color-map');
 
 describe('ColorMap', function() {
@@ -73,6 +74,93 @@ describe('ColorMap', function() {
 				instance.color('A', '#ff000');
 			}).toThrowError();
 		});
+	});
+
+	describe('raw data source', function() {
+
+		var sourceData;
+
+		beforeEach(function() {
+			sourceData = {
+				"0": "#fc0707",
+				"1": "#050405",
+				"2": "#efd20b",
+				"3": "#1db815",
+				"4": "#e9503b",
+				"5": "#0617f6",
+				"6": "#14aa0e",
+				"7": "#891190",
+				"8": "#ec570a",
+				"9": "#91f07a",
+				"A": "#f20707",
+				"B": "#f87a06",
+				"C": "#e6f207",
+				"D": "#f20d0d",
+				"E": "#e89c58",
+				"F": "#92430a",
+				"G": "#f26960",
+				"H": "#fb9b1c",
+				"I": "#3b0446",
+				"J": "#03ae19",
+				"K": "#0c15f4",
+				"L": "#980dae",
+				"M": "#b696b9",
+				"N": "#e0a35c",
+				"O": "#f61009",
+				"P": "#41e638",
+				"Q": "#f6096d",
+				"R": "#1f9e05",
+				"S": "#f2f706",
+				"T": "#823307",
+				"U": "#f3dd14",
+				"V": "#24bc11",
+				"W": "#0b0df6",
+				"X": "#762908",
+				"Y": "#968e8e",
+				"Z": "#1206f9"
+			};
+		});
+
+		describe('.fromData', function() {
+
+			var sourceDataBackup, instance;
+
+			beforeEach(function() {
+				sourceDataBackup = _.clone(sourceData);
+				// sanity check
+				expect(sourceDataBackup).toEqual(sourceData);
+				instance = ColorMap.fromData(sourceData);
+			});
+
+			it('should preserve source data', function() {
+				expect(sourceData).toEqual(sourceDataBackup);
+			});
+
+			it('should have correct color for A', function() {
+				expect(instance.color('A')).toEqual('#f20707');
+			})
+
+			it('should have correct color for every grapheme from source data', function() {
+				_.each(sourceData, function(color, grapheme) {
+					expect(instance.color(grapheme)).toEqual(color);
+				});
+			});
+
+			describe('.toData', function() {
+				var data;
+				beforeEach(function() {
+					data = instance.toData();
+				});
+				it('should output equivalent data', function() {
+					expect(data).toEqual(sourceData);
+				});
+				it('should dereference output data', function() {
+					expect(data).not.toBe(sourceData);
+				});
+			});
+
+		});
+
 	});
 
 });
